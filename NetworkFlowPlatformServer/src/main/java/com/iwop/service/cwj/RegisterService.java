60be04927f4021871f69e324db69ce36f1b0b4cb
liuyunmoke <liuyunmoke@gmail.com>
2016-04-28 15:45:06 +0800

    流量平台web

---
+++ NetworkFlowPlatformServer/src/main/java/com/iwop/service/cwj/RegisterService.java
@@ -0,0 +1,16 @@
+package com.iwop.service.cwj;
+
+import com.pipipark.j.mvc.PPPMvcService;
+
+
+
+@SuppressWarnings("serial")
+public class RegisterService implements PPPMvcService {
+	
+	
+	public String execute(){
+//		Database.jdbc().
+		return "";
+	}
+	
+}
